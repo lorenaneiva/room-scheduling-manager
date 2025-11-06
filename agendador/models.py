09d9ebd0284e332babab13f20f0c9424f2bf043c
Lorena Gama Neiva <lorenaneiva2232820@gmail.com>
2025-11-06 15:57:34 -0300

    first commit

---
+++ agendador/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    ROLE_CHOICES = [
+        ('professor','Professor'),
+        ('administrador','Administrador'),
+    ]
+    role = models.CharField(max_length=20,choices=ROLE_CHOICES, default='professor')
+    def __str__(self):
+        return self.username
+
+
+class Sala(models.Model):
+
+    nome = models.CharField(max_length=200)
+    capacidade = models.IntegerField()
+    recursos = models.TextField(blank=True)
+    criado_em = models.DateTimeField(auto_now_add=True)
+    localização = models.CharField(max_length=300)
+    def __str__(self):
+        return self.nome
+
+class Agendamento(models.Model):
+    STATUS_CHOICES = [
+        ('pendente', 'Pendente'),
+        ('aceito', 'Aceito'),
+        ('rejeitado', 'Rejeitado'),
+        ('cancelado', 'Cancelado')
+    ]
+
+    professor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='agendamentos')
+    materia = models.CharField(max_length=200)
+    observação = models.TextField()
+    status = models.CharField(choices=STATUS_CHOICES, default='pendente')
+    sala = models.ForeignKey(Sala, on_delete=models.CASCADE, related_name="salas")
+    data_agendamento = models.DateField()
+    hora_inicio = models.TimeField()
+    hora_fim = models.TimeField()
+
+    def __str__(self):
+        return f"{self.sala.nome} - {self.data_agendamento} - {self.professor.username}"
+    def clean(self):
+        if self.hora_inicio > self.hora_fim: 
+            raise ValidationError("A hora inicial não pode ser depois da hora final.")
