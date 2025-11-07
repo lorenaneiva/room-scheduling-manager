1b5bac89d4b61e50dea600ebdc40117668316e20
Lorena Gama Neiva <lorenaneiva2232820@gmail.com>
2025-11-07 16:42:50 -0300

    criando rota para professor agendar uma sala em especifico

--- agendador/urls.py
+++ agendador/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
-    path('salas', views.salas, name='salas')
+    path('salas', views.salas, name='salas'),
+    path('agendar', views.agendar, name='agendar')
 ]
