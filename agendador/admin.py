ebdd21f936ba5d21b4a7821601ef7cb2b0550eed
Lorena Gama Neiva <lorenaneiva2232820@gmail.com>
2025-11-07 15:32:16 -0300

    mudando modelo de usuario, para as migrações funcionarem

--- agendador/admin.py
+++ agendador/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from agendador.models import User, Agendamento, Sala
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Agendamento)
+admin.site.register(Sala)
