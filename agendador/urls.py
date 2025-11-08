ebdb38786facd556cdc2e7114e33901c17a60960
Lorena Gama Neiva <lorenaneiva2232820@gmail.com>
2025-11-08 14:21:25 -0300

    feat: adiciona url para aceitar ou recusar agendamentos

--- agendador/urls.py
+++ agendador/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('salas', views.salas, name='salas'),
-    path('agendar/<sala_id>/', views.agendar, name='agendar')
+    path('agendar/<sala_id>/', views.agendar, name='agendar'),
+    path('agendamentos_pendentes', views.agendamentos_pendentes, name='agendamentos_pendentes')
 ]
