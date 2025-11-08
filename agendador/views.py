6e6c7c2824efc337442e6751f2ef9fca4b1f7a22
Lorena Gama Neiva <lorenaneiva2232820@gmail.com>
2025-11-07 23:03:33 -0300

    criação da views para agendar uma sala em especifico, atribuindo os valores aos campos vazios deixado pelo forms

--- agendador/views.py
+++ agendador/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from .models import User, Agendamento, Sala
-from datetime import date
+from .forms import AgendamentoForm
+from datetime import date, timedelta
+from django.db import transaction
+from django.contrib import messages
+from django.shortcuts import get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 def index(request):
     data_atual = date.today()
@@ -24,3 +31,25 @@ def salas(request):
     }
 
     return render(request,'agendador/salas.html', context)
+
+def agendar(request, sala_id):
+    if request.method != 'POST':
+        form = AgendamentoForm()
+    else:
+        form = AgendamentoForm(request.POST)
+        if form.is_valid():
+            with transaction.atomic():
+                novo_agendamento = form.save(commit=False)
+                novo_agendamento.professor = request.user
+                novo_agendamento.sala = get_object_or_404(Sala, id=sala_id)
+                hoje = date.today()
+                amanha = hoje + timedelta(days=1)
+                novo_agendamento.data_agendamento = amanha 
+                novo_agendamento.save()
+            messages.success(request, 'Solicitação de agendamento feita com sucesso!')
+            return HttpResponseRedirect(reverse('salas'))
+    context = {
+        'form': form,
+        'sala_id':sala_id
+    }
+    return render(request,'agendador/agendar.html', context)
