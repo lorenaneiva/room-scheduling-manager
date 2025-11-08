2d6caae6fcd5d1790dac0079dc05aadf00f9cfb0
Lorena Gama Neiva <lorenaneiva2232820@gmail.com>
2025-11-07 23:02:16 -0300

    formulario para criação de agendamentos com validações nas horas

--- agendador/forms.py
+++ agendador/forms.py
@@ -1,14 +1,35 @@
 from django import forms
 from .models import Agendamento
+from django.forms import widgets
+from django.core.exceptions import ValidationError
+from datetime import datetime, timedelta 
 
 class AgendamentoForm(forms.ModelForm):
-    hora_inicio, hora_fim = forms.DateField(
-        required=False, 
-        widget=forms.DateInput(attrs={'type':'date'}, format='%Y-%m-%d'),
-        input_formats=['%Y-%m-%d'],
-        label='Prazo')
+    hora_fim = forms.TimeField(
+        widget=widgets.TimeInput(attrs={'type': 'time'}),
+        label='Hora final')
+    hora_inicio = forms.TimeField(
+        widget=widgets.TimeInput(attrs={'type': 'time'}),
+        label='Hora inicial')
     
     class Meta():
         model = Agendamento
         fields = ['hora_inicio','hora_fim','materia','observação']
-        labels = {'hora_inicio':'Hora inicial','hora_fim': 'Hora final', 'materia':'Matéria', 'observação': 'Observação'}
+        labels = {'materia':'Matéria', 'observação': 'Observação'}
+
+    def clean(self):
+        # clean da classe pai
+        cleaned_data = super().clean()
+        hora_i = cleaned_data.get('hora_inicio')
+        hora_f = cleaned_data.get('hora_fim')
+
+        if hora_i and hora_f:
+            inicio_segundos = hora_i.hour * 3600 + hora_i.minute * 60 + hora_i.second
+            fim_segundos = hora_f.hour * 3600 + hora_f.minute * 60 + hora_f.second
+            duracao = fim_segundos - inicio_segundos
+            if hora_i >= hora_f:
+                raise ValidationError("A hora inicial deve ser antes da hora final.")
+            if duracao < 3600:
+                raise ValidationError("A reserva da sala deve ser de pelo menos 1h.")
+            if duracao > 14400:
+                raise ValidationError("A reserva da sala deve ser de no máximo 4h.")
