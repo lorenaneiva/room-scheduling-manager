c2472e931047bf92f9b0d9274cb2a19447dbbaec
Lorena Gama Neiva <lorenaneiva2232820@gmail.com>
2025-11-08 13:23:53 -0300

    expondo o dia do agendamento na pagina agendar

--- agendador/views.py
+++ agendador/views.py
@@ -25,9 +25,8 @@ def index(request):
 
 def salas(request):
     salas = Sala.objects.order_by('capacidade')
-
     context = {
-        'salas':salas
+        'salas':salas,
     }
 
     return render(request,'agendador/salas.html', context)
@@ -38,18 +37,34 @@ def agendar(request, sala_id):
     else:
         form = AgendamentoForm(request.POST)
         if form.is_valid():
+            
+            novo_agendamento = form.save(commit=False)
+            novo_agendamento.professor = request.user
+            novo_agendamento.sala = get_object_or_404(Sala, id=sala_id)
+            novo_agendamento.data_agendamento = date.today() + timedelta(days=1)
+        
+            conflito = Agendamento.objects.filter(
+                sala=novo_agendamento.sala,
+                data_agendamento=novo_agendamento.data_agendamento,
+                status='aceito'
+            ).filter(
+                hora_inicio__lt=novo_agendamento.hora_fim, # hora_inicio é menor que a hora_fim
+                hora_fim__gt=novo_agendamento.hora_inicio # hora_fim é maior que a hora_inicio
+            ).exists()
+            if conflito:
+                messages.error(request, "Essa sala já está reservada nesse horário.")
+                return render(request, 'agendador/agendar.html', {'form': form, 'sala_id': sala_id})
+            
             with transaction.atomic():
-                novo_agendamento = form.save(commit=False)
-                novo_agendamento.professor = request.user
-                novo_agendamento.sala = get_object_or_404(Sala, id=sala_id)
-                hoje = date.today()
-                amanha = hoje + timedelta(days=1)
-                novo_agendamento.data_agendamento = amanha 
                 novo_agendamento.save()
             messages.success(request, 'Solicitação de agendamento feita com sucesso!')
             return HttpResponseRedirect(reverse('salas'))
+    hoje = date.today()
+    amanha = hoje + timedelta(days=1)
+    
     context = {
         'form': form,
-        'sala_id':sala_id
+        'sala_id':sala_id,
+        'amanha':amanha
     }
     return render(request,'agendador/agendar.html', context)
