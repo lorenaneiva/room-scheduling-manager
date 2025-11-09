dfc1b26eade636cccfa9ee4b3689da957c62ae23
Lorena Gama Neiva <lorenaneiva2232820@gmail.com>
2025-11-09 14:25:15 -0300

    resolve erro em models, onde status nao estava com max_length definido

--- agendador/models.py
+++ agendador/models.py
@@ -33,7 +33,7 @@ class Agendamento(models.Model):
     professor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='agendamentos')
     materia = models.CharField(max_length=200)
     observação = models.TextField()
-    status = models.CharField(choices=STATUS_CHOICES, default='pendente')
+    status = models.CharField(choices=STATUS_CHOICES, default='pendente', max_length=30)
     sala = models.ForeignKey(Sala, on_delete=models.CASCADE, related_name="salas")
     data_agendamento = models.DateField()
     hora_inicio = models.TimeField()
