50ee53015da4159a548e1442e9813e5f990538ca
Lorena Gama Neiva <lorenaneiva2232820@gmail.com>
2025-11-08 23:21:32 -0300

    cria rota e template para login de usuario

---
+++ users/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login')
+]
