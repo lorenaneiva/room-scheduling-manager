a6c4cf571cc426dc6cd68c9ba16c5b9abaa8b9ea
Lorena Gama Neiva <lorenaneiva2232820@gmail.com>
2025-11-08 23:46:06 -0300

    adiciona a função de registro ao sistema

--- users/views.py
+++ users/views.py
@@ -2,7 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth import logout
-
+from .forms import RegistroForm
+from django.contrib import messages
+from django.shortcuts import redirect
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse('index'))
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistroForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Usuário criado com sucesso! Faça login.')
+            return redirect('login')
+    else:
+        form = RegistroForm()
+    return render(request, 'users/register.html', {'form': form})
