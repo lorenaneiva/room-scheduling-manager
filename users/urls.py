4f7883d3f68a5a93984d67e34e7ed3e3afbccd8b
Lorena Gama Neiva <lorenaneiva2232820@gmail.com>
2025-11-08 23:24:26 -0300

    cria a rota e view para logout

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from . import views
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login')
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', views.logout_view, name='logout')
 ]
